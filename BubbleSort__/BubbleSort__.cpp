96a3d8261406e9e7655d906f11c5fa5465d15d2e
hilal555 <muhamadhilal04@gmail.com>
2023-03-17 13:28:42 +0700

    Menambahkan variabel

--- BubbleSort__/BubbleSort__.cpp
+++ BubbleSort__/BubbleSort__.cpp
@@ -1,6 +1,6 @@
-
-
 #include <iostream>
+int a[20];              //Deklarasi array a dengan ukuran 20
+int n;                  //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 int main()
 {
